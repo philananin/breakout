61178765a99de1e14c6c2e31c9e30e71c7ae3e82
Phil Ananin <phil.ananin@gmail.com>
2014-05-28 08:35:26 +0100

    bit of a tidy up

--- main.py
+++ main.py
@@ -21,11 +21,21 @@ class Ball:
     def change_direction(self):
         self.touching_paddle = False
         self.touching_block = None
+        self.direction = 1-self.direction
 
-        if self.direction == Direction.UP:
-            self.direction = Direction.DOWN
-        else:
-            self.direction = Direction.UP
+    def update(self, paddle, blocks):
+        if self.touching_block is not None:
+            self.remove_touching_block()
+            self.change_direction()
+
+        if self.touching_paddle:
+            self.change_direction()
+
+        self.move()
+        self.check_collision(paddle, blocks)
+
+    def remove_touching_block(self):
+        self.touching_block.alive = False
 
     def move(self):
         if self.direction == Direction.UP:
@@ -35,24 +45,18 @@ class Ball:
 
     def check_collision(self, paddle, blocks):
         if self.direction == Direction.UP:
-            if (self.x, self.y-1) in blocks:
-                self.touching_block = blocks[(self.x, self.y-1)]
-            else:
-                self.touching_block = None
+            self.touching_block = blocks.get((self.x, self.y-1), None)
         else:
-            if paddle.touches(self.x, self.y):
-                self.touching_paddle = True
-            else:
-                self.touching_paddle = False
+            self.touching_paddle = paddle.is_in_contact_with(self.x, self.y)
 
 class Paddle:
     def __init__(self, x, y):
-        self.width = x # for now cover whole of bottom row
-        self.x = 0
+        self.width = x//5
+        self.x = x//2-self.width//2
         self.y = y
 
-    def touches(self, x, y):
-        y+1 == self.y
+    def is_in_contact_with(self, x, y):
+        self.y-1 == y
 
 class Game:
     def __init__(self, x, y):
@@ -60,14 +64,13 @@ class Game:
         self.blocks_start_at_row = y // 2
         self.add_blocks(x, self.blocks_start_at_row)
         self.in_play = False
-        self.paddle = Paddle(x-1, y-1)
-
+        self.paddle = Paddle(x, y-1)
         # ball starts in the middle of the first row up
         self.ball = Ball(x // 2, y-2, Direction.UP)
 
     def add_blocks(self, cols, rows):
         self.blocks = {}
-        for x in range(0, cols - 1):
+        for x in range(0, cols):
             for y in range(0, rows):
                 self.blocks[(x, y)] = Block()
 
@@ -76,16 +79,10 @@ class Game:
             self.in_play = True
 
     def update(self):
-        if self.ball.touching_block is not None:
-            self.remove_touching_block()
-            self.ball.change_direction()
-
-        if self.ball.touching_paddle:
-            self.ball.change_direction()
-
+        # also need to handle touching the edges
+        # and resetting if we touch the bottom
         if self.in_play:
-            self.ball.move()
-            self.ball.check_collision(self.paddle, self.blocks)
+            self.ball.update(self.paddle, self.blocks)
 
     def remove_touching_block(self):
         self.ball.touching_block.alive = False
@@ -93,6 +90,7 @@ class Game:
     def render(self, screen):
         self.render_ball(screen)
         self.render_blocks(screen)
+        self.render_paddle(screen)
         screen.refresh()
 
     def render_ball(self, screen):
@@ -109,6 +107,9 @@ class Game:
                 screen.addstr(pos[1], pos[0], '#')
             else:
                 screen.addstr(pos[1], pos[0], ' ')
+
+    def render_paddle(self, screen):
+        screen.addstr(self.paddle.y, self.paddle.x, '='*self.paddle.width)
 
 def main(screen):
     screen.nodelay(1) # user input is non-blocking
