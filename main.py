25a606c99fa22ac6bfe6ff5bc7167fb2d2dca1ad
Phil Ananin <phil.ananin@gmail.com>
2014-06-10 22:57:18 +0100

    some more tidying up

--- main.py
+++ main.py
@@ -13,23 +13,24 @@ handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 class Block(object):
-    def __init__(self, pos, sym, color, on_hit):
-        self.pos = pos
+    def __init__(self, x, y, sym, color, on_hit):
+        self.x = x
+        self.y = y
         self.on_hit = on_hit
         self.color = color
         self.sym = sym
 
     def hit(self):
-        self.on_hit((self.pos.x, self.pos.y))
+        self.on_hit((self.x, self.y))
 
     def render(self, screen):
         max_y, max_x = screen.getmaxyx()
-        if self.pos.x == max_x and self.pos.y == max_y:
+        if self.x == max_x and self.y == max_y:
             char = screen.getch(max_y, max_x - 1)
             screen.addstr(max_y, max_x - 1, self.sym)
             screen.insstr(max_y, max_x - 1, char)
         else:
-            screen.addstr(self.pos.y, self.pos.x, self.sym, self.color)
+            screen.addstr(self.y, self.x, self.sym, self.color)
 
 class Vec2(object):
     def __init__(self, x, y):
@@ -57,38 +58,31 @@ class Ball:
 
     def update(self, paddle, blocks, dt):
         def get_new_position():
-            logger.info('direction_vec: %f, %f', self.direction_vec.x, self.direction_vec.y)
-            scaled = self.direction_vec.scale(dt * self.speed)
-            logger.info('scaled: %f, %f', scaled.x, scaled.y)
-            return self.pos.translate(scaled)
+            scaled_dir = self.direction_vec.scale(dt * self.speed)
+            return self.pos.translate(scaled_dir)
 
         # todo: technically we could cross more than one cell...
         def get_crossed_cell(old_pos, new_pos):
-            crossed_x = math.floor(new_pos.x) != math.floor(old_pos.x)
-            crossed_y = math.floor(new_pos.y) != math.floor(old_pos.y)
-
+            current_x = math.floor(new_pos.x)
+            current_y = math.floor(new_pos.y)
+            crossed_x = current_x != math.floor(old_pos.x)
+            crossed_y = current_y != math.floor(old_pos.y)
             if crossed_x and crossed_y:
-                return (Vec2(math.floor(new_pos.x), math.floor(new_pos.y)),
-                        Plane.BOTH)
+                return (Vec2(current_x, current_y), Plane.BOTH)
             if crossed_x:
-                return (Vec2(math.floor(new_pos.x), math.floor(new_pos.y)),
-                        Plane.Y)
+                return (Vec2(current_x, current_y), Plane.Y)
             if crossed_y:
-                return (Vec2(math.floor(new_pos.x), math.floor(new_pos.y)),
-                        Plane.X)
+                return (Vec2(current_x, current_y), Plane.X)
 
         def get_hit_block_and_crossed_plane(cell):
-            logger.info('get_hit_block_and_crossed_plane called')
             block = blocks.get((cell[0].x, cell[0].y), None)
             if block:
-                logger.info('block found: %s', block)
                 return (block, cell[1])
             if paddle.occupies_cell(cell[0].x, cell[0].y):
-                logger.info('paddle touches!')
                 return (paddle, Plane.X)
 
         # todo
-        def get_time_to_hit(hit_pos):
+        def get_time_to_hit(hit_cell):
             return 0
 
         logger.info('dt: %d', dt)
@@ -96,13 +90,7 @@ class Ball:
         new_pos = get_new_position()
         logger.info('new pos: %f, %f', new_pos.x, new_pos.y)
 
-        # starting at a floored position makes this inaccurate, but a simple
-        # implementation for now
-        floored_x = int(math.floor(self.pos.x))
-        floored_y = int(math.floor(self.pos.y))
-        logger.info('floored_x: %d, floored_y: %d', floored_x, floored_y)
         crossed_cell = get_crossed_cell(self.pos, new_pos)
-
         if not crossed_cell:
             logger.info('no crossed cells')
             self.pos = new_pos
@@ -112,7 +100,7 @@ class Ball:
                 logger.info('no hit block')
                 self.pos = new_pos
             else:
-                logger.info('HIT BLOCK!')
+                logger.info('hit block')
                 hit_block, plane = result
                 time_to_hit = 0 # todo - paddle doesn't work with pos: get_time_to_hit(hit_block.pos)
                 time_remaining = dt - time_to_hit
@@ -121,9 +109,6 @@ class Ball:
                 self.update(paddle, blocks, time_remaining)
 
     def bounce(self, plane):
-        logger.info('bouncing! in plane %s', plane)
-        logger.info('angle: %f', self.angle)
-        logger.info('direction: %f, %f', self.direction_vec.x, self.direction_vec.y)
         if plane == Plane.X or plane == Plane.BOTH:
             self.angle = -self.angle
         if plane == Plane.Y or plane == Plane.BOTH:
@@ -131,10 +116,7 @@ class Ball:
                 self.angle = math.pi - self.angle
             else:
                 self.angle = -math.pi - self.angle
-        logger.info('new angle: %f', self.angle)
-        self.direction_vec = Vec2(math.cos(self.angle),
-                                  math.sin(self.angle))
-        logger.info('new direction: %f, %f', self.direction_vec.x, self.direction_vec.y)
+        self.direction_vec = Vec2(math.cos(self.angle), math.sin(self.angle))
 
 class Paddle:
     def __init__(self, x, y):
@@ -170,25 +152,23 @@ class Game(object):
     def add_blocks(self, width, height):
         self.add_border(width, height)
         yellow = curses.color_pair(1)
-
         for x in range(2, width - 3):
             for y in range(2, height // 2):
-                self.blocks[(x, y)] = Block(Vec2(x, y), '%', yellow, self.blocks.pop)
+                self.blocks[(x, y)] = Block(x, y, '%', yellow, self.blocks.pop)
 
     def add_border(self, width, height):
-        do_nothing = lambda x: None
+        noop = lambda x: None
         green = curses.color_pair(2)
 
-        self.blocks[(0, 0)] = Block(Vec2(0, 0), '╔', green, do_nothing)
+        self.blocks[(0, 0)] = Block(0, 0, '╔', green, noop)
+        self.blocks[(width - 1, 0)] = Block(width - 1, 0, '╗', green, noop)
 
         for x in range(1, width - 1):
-            self.blocks[(x, 0)] = Block(Vec2(x, 0), '═', green, do_nothing)
-
-        self.blocks[(width - 1, 0)] = Block(Vec2(width - 1, 0), '╗', green, do_nothing)
+            self.blocks[(x, 0)] = Block(x, 0, '═', green, noop)
 
         for y in range (1, height - 1):
-            self.blocks[(0, y)] = Block(Vec2(0, y), '║', green, do_nothing)
-            self.blocks[(width - 1, y)] = Block(Vec2(width - 1, y), '║', green, do_nothing)
+            self.blocks[(0, y)] = Block(0, y, '║', green, noop)
+            self.blocks[(width - 1, y)] = Block(width - 1, y, '║', green, noop)
 
     def handle_input(self, user_input):
         if user_input == ord(' '):
